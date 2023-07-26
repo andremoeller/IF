0be5484e03c5356653799efe16de0c3067b4a772
André Kang-Moeller <akm@octoml.ai>
2023-07-26 11:21:11 -0700

    Commit before migration

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 cog build
-docker build -t r8.im/andremoeller/deep-floyd --build-arg HUGGINGFACE_API_KEY=$HUGGINGFACE_API_TOKEN .
+docker build -t r8.im/andremoeller/deep-floyd --build-arg HUGGINGFACE_TOKEN=$HUGGINGFACE_API_TOKEN .
 docker push r8.im/andremoeller/deep-floyd
